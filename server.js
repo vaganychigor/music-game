8c05d5a21aa4321d31947b580c2eac8c214f58ed
vaganychigor <igorvlassiev@gmail.com>
2025-06-17 03:49:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Обслуживаем статические файлы
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  console.log('Клиент подключён:', socket.id);
+  console.log('Клиент подключился');
 
-  socket.on('play-song', (data) => {
-    socket.broadcast.emit('play-song', data);
+  socket.on('playTrack', (trackName) => {
+    console.log(`Проигрывается: ${trackName}`);
+    // Отправка всем клиентам, кроме отправителя
+    socket.broadcast.emit('playTrack', trackName);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Сервер работает на порту ' + PORT));
+http.listen(PORT, () => {
+  console.log(`Сервер запущен: http://localhost:${PORT}`);
+});

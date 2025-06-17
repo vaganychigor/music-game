7399d8f4d79168ba681426c3818ba5c2f0b8ac40
vaganychigor <igorvlassiev@gmail.com>
2025-06-17 11:49:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-// server.js
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Статические файлы из папки public
 app.use(express.static('public'));
 
-// Обработка подключений по WebSocket
-io.on('connection', (socket) => {
-  console.log('Клиент подключился');
-
-  socket.on('playTrack', (trackName) => {
-    console.log(`Хост запускает: ${trackName}`);
-    // Отправить ВСЕМ остальным клиентам сигнал на проигрывание
-    socket.broadcast.emit('playTrack', trackName);
+io.on('connection', socket => {
+  socket.on('playTrack', file => {
+    socket.broadcast.emit('playTrack', file);
   });
 });
 
-// Запуск сервера
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Сервер работает на порту ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
